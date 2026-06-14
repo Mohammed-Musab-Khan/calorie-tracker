6e1e94ef616372a7eee66a002eb80360d5458600
Mohammed Musab Khan <khanmusab51@gmail.com>
2026-06-15 00:00:04 +0530

    feat: Calorie Tracker using use state

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,71 @@
+"use client"
 import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Field, FieldContent, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { useState } from "react"
+
+interface FoodCals {
+  food: string,
+  calories: number
+}
 
 export default function Page() {
+
+  const [food, setFood] = useState<string>("")
+  const [calories, setCalories] = useState<number>(0)
+
+  const [foodCals, setfoodCals] = useState<FoodCals[]>([])
+
+  const AddItem = (item:FoodCals) => {
+    setfoodCals([...foodCals,{food:item.food, calories:item.calories}])
+  }
+
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
+    <div className="p-6 justify-items-center">
+      <Card className="">
+        <CardHeader>
+          <CardTitle>Calorie Tracker</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="food-item">Food Item</FieldLabel>
+              <Input id="food-item" placeholder="Enter Food Item" type="text" value={food} onChange={(e) => setFood(e.target.value)} />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="calories">Calories</FieldLabel>
+              <Input id="calories" placeholder="Calories" type="number" value={calories} onChange={(e) => setCalories(Number(e.target.value))} />
+            </Field>
+            <Button onClick={() => AddItem({food:food, calories:calories})} >Add Food</Button>
+          </FieldGroup>
+          <Separator className="my-3" />
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableHead>Food Item</TableHead>
+                <TableHead>Calories</TableHead>
+                <TableHead>Action</TableHead>
+              </TableRow>
+              {
+                foodCals.map(item => (
+                   <TableRow key={item.food}>
+                    <TableCell>{item.food}</TableCell>
+                    <TableCell>{item.calories}</TableCell>
+                    <TableCell>Edit</TableCell>
+                  </TableRow>
+                ))
+              }
+            </TableBody>
+            <TableFooter></TableFooter>
+          </Table>
+        </CardContent>
+        <CardFooter>
+          <CardTitle>Total Calories 0</CardTitle>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
